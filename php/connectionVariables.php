5f9044d1e5fba294e41c80a52294fab790c64451
Alexander Koch <alexander.koch@ugent.be>
2015-05-20 17:27:06 +0200

    Added the connection variables file.

--- php/connectionVariables.php
+++ php/connectionVariables.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','phpmysql');
-define('DB_NAME','mexpressdb_tcga');
+define('DB_HOST','');
+define('DB_USER','');
+define('DB_PASSWORD','');
+define('DB_NAME','mexpress');
 
 ?>
