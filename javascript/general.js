93b1a829c9de8071416e8d73e6d331a5c67be18b
Alexander Koch <alexander.koch@ugent.be>
2015-11-25 15:22:29 +0100

    Merged js files

--- javascript/general.js
+++ javascript/general.js
@@ -22,5 +22,35 @@ $(document).ready(function() {
     inputWidth = $('.userInput').outerWidth();
     plotWidth = windowWidth - inputWidth - 28;
     $('.plotContainer').width(plotWidth);
+
+    // autocomplete functionality
+    var options, a;
+    $(function(){
+        
+        options = {
+            serviceUrl:'php/autocomplete.php',
+            minChars:1,
+            maxHeight:200,
+            deferResquestBy:0
+        };
+        a = $('#gene').autocomplete(options);
+        
+    });
+
+    // sample selection
+    $(".sampleSelectionElement").click(function() {
+        
+        var clickedElement = $(this);
+        // remove the selected class for every selected element
+        $(".sampleSelectionElement").each(function() {
+            
+            if ($(this).hasClass("selectedSample")) {
+                $(this).toggleClass("selectedSample");
+            }
+            
+        });
+        clickedElement.toggleClass("selectedSample");
+        
+    });
     
 });
